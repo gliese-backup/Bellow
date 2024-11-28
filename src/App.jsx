b95afd2dc105885c6de735681b6444d45c77cd9a
gliese-backup <glieseb+1@proton.me>
2024-11-28 13:52:11 +0000

    Center the content

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,22 @@
 function App() {
   return (
-    <div className="App">
-      <h1>Hello Bellow</h1>
+    <div className="flex items-center justify-center h-screen">
+      <div>
+        <details>
+          <summary>Hello Bellow</summary>
+          <p>This is a demo of Bellow</p>
+        </details>
+        <details>
+          <summary>Hello Bellow</summary>
+          <p>This is a demo of Bellow</p>
+        </details>
+        <details>
+          <summary>Hello Bellow</summary>
+          <p>This is a demo of Bellow</p>
+        </details>
+      </div>
     </div>
   );
-}   
+}
 
 export default App;
