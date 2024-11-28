9665904447d8782c415021205a02ac63585ae07d
gliese-backup <glieseb+1@proton.me>
2024-11-28 13:54:43 +0000

    Bellow component create

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,9 @@
+import Bellow from "./Bellow";
+
 function App() {
   return (
     <div className="flex items-center justify-center h-screen">
-      <div>
-        <details>
-          <summary>Hello Bellow</summary>
-          <p>This is a demo of Bellow</p>
-        </details>
-        <details>
-          <summary>Hello Bellow</summary>
-          <p>This is a demo of Bellow</p>
-        </details>
-        <details>
-          <summary>Hello Bellow</summary>
-          <p>This is a demo of Bellow</p>
-        </details>
-      </div>
+      <Bellow />
     </div>
   );
 }
---
+++ src/Bellow.jsx
@@ -0,0 +1,16 @@
+function Bellow() {
+  return (
+    <div>
+      <details>
+        <summary>Changed</summary>
+        <p>This is a demo of Bellow</p>
+      </details>
+      <details>
+        <summary>Hello Bellow</summary>
+        <p>This is a demo of Bellow</p>
+      </details>
+    </div>
+  );
+}
+
+export default Bellow;
